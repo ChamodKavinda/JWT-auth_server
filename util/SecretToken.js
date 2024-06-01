9419b2b0454ee806b5a5f09c9a483d56b6add970
ChamodKavinda <kavindachamod100@gmail.com>
2024-06-01 20:07:04 +0530

    implement login function

--- util/SecretToken.js
+++ util/SecretToken.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 const jwt = require("jsonwebtoken");
+const {TOKEN_KEY} = process.env.TOKEN_KEY
 
 module.exports.createSecretToken=(id)=>{
     return jwt.sign({id},process.env.TOKEN_KEY,{
