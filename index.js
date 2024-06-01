49543b8f688d69f5120e09c00b8c57d70c30a3da
ChamodKavinda <kavindachamod100@gmail.com>
2024-06-01 13:10:19 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const bodyParser = require('body-parser')
+
+const PORT=4000;
+
+
+const app = express();
+
+app.use(cors());
+
+app.use(bodyParser.urlencoded({ extended: false }))
+
+
+app.use(bodyParser.json())
+
+mongoose.connect('mongodb://localhost:27017/Auth').then(()=>{
+    app.listen(PORT, ()=>{
+        console.log(`Node server is listening to ${PORT}`)
+    });
+}).catch(error=>{
+    console.log('Mongodb Error',error);
+})
+
+app.use(
+    cors({
+        origin: ["http://localhost:4000"],
+        methods: ["GET", "POST", "PUT", "DELETE"],
+        credentials: true,
+    })
+);
+
+app.use(express.json());
